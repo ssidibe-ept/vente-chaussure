45b9b0aa3bbc0606bc738d47f93d5f9066b782bc
thiombane2006 <thiombanebayemarame@gmail.com>
2026-02-13 10:13:59 +0000

    Ajout entite chaussure

---
+++ src/main/java/sn/edu/isepat/vente_chaussure/entities/Chaussure.java
@@ -0,0 +1,25 @@
+package sn.edu.isepat.vente_chaussure.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "chaussure")
+
+public class Chaussure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100, nullable = false)
+    private String nom;
+
+    @Column(nullable = false)
+    private Double prix;
+
+    private String description;
+
+}
---
+++ src/main/java/sn/edu/isepat/vente_chaussure/repositories/VenteChaussureApplication.java
@@ -0,0 +1,5 @@
+package sn.edu.isepat.vente_chaussure.repositories;
+
+public class VenteChaussureApplication {
+
+}
